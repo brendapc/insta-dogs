c2ffc7075c0c3ca2362a09e7909c27ba51c7f197
Brenda Pereira <brendacamara@outlook.com>
2021-01-28 16:00:26 -0300

    infinite scroll

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import FeedPhotosItem from './FeedPhotosItem';
-import useFetch from '../../Hooks/useFetch';
-import { PHOTOS_GET } from '../../Api';
-import Error from '../Helper/Error';
-import Loading from '../Helper/Loading';
-import styles from './FeedPhotos.module.css';
+import React from "react";
+import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../Api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({
+  page = 1,
+  user,
+  setModalPhoto,
+  setInfinite,
+  total = 3,
+}) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
+      const { url, options } = PHOTOS_GET({ page, total: 3, user });
+      const { json, response } = await request(url, options);
       console.log(json);
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, total, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
