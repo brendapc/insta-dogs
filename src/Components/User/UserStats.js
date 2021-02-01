fb543334afc83be2be6fdf6d447393b61811c2de
Brenda Pereira <brendacamara@outlook.com>
2021-02-01 19:04:52 -0300

    counting visualizations

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { STATS_GET } from "../../Api";
 import Head from "../Helper/Head";
+import useFetch from "../../Hooks/useFetch";
+import Loading from "../Helper/Loading";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
